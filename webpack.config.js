17918df55f4a267086c0367a306a4e03c8f0df74
KrlozMedina <camedinal@correo.udistrital.edu.co>
2022-01-25 21:23:19 -0500

    Curso 4, agregando stilos

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   // mode: 'development',
@@ -26,6 +27,14 @@ module.exports = {
           { loader: 'html-loader'}
         ]
       },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
       // {
       //   test: /\.m?js$/,
       //   exclude: /node_modules/,
@@ -42,6 +51,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
     })
   ],
   devServer: {
